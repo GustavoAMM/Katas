bccf20927ad44e1577f253932a5772b37afeddd4
Gustavo Montoya <99113181+GustavoAMM@users.noreply.github.com>
2022-02-13 22:34:23 -0600

    add kata 9

--- Generacion.py
+++ Generacion.py
@@ -15,10 +15,7 @@ def water_left(astronauts, water_left, days_left):
         raise RuntimeError(f"There is not enough water for {astronauts} astronauts after {days_left} days!")
     return f"Total water left after {days_left} days is: {total_water_left} liters"
 
-try:
-    water_left(5, 100, 2)
-except RuntimeError as err:
-    alert_navigation_system(err)
+
 
 def water_left(astronauts, water_left, days_left):
     for argument in [astronauts, water_left, days_left]:
